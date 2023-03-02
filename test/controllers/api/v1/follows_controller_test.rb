a7c37f7fd47e5c5c82a78115c9e1dd2b6cbe0101
Zahid Ali <zahid313@gmail.com>
2023-03-03 08:48:00 +0900

    Fixed failing tests in Follow Controller
    - Reason was we were using show to render response during POST request but we had no show action

--- test/controllers/api/v1/follows_controller_test.rb
+++ test/controllers/api/v1/follows_controller_test.rb
@@ -5,15 +5,15 @@ class Api::V1::FollowsControllerTest < ActionDispatch::IntegrationTest
     user = User.create(name: "John")
     other_user = User.create(name: "Jane")
     assert_difference('Follow.count') do
-      post api_v1_follows_path, params: { user_id: user.id, id: other_user.id}
+      post api_v1_follows_path, params: { user_id: user.id, id: other_user.id}, as: :json
     end
     assert_response :success
-  end
+  end 
 
   test "should not follow invalid user" do
     user = User.create(name: "John")
     assert_no_difference('Follow.count') do
-      post api_v1_follows_path, params: { user_id: user.id, id: 999}
+      post api_v1_follows_path, params: { user_id: user.id, id: 999}, as: :json
     end
     assert_response :not_found
   end
@@ -23,7 +23,7 @@ class Api::V1::FollowsControllerTest < ActionDispatch::IntegrationTest
     other_user = User.create(name: "Jane")
     follow = Follow.create(follower_id: user.id, following_id: other_user.id)
     assert_difference('Follow.count', -1) do
-      delete api_v1_follow_path(other_user.id), params: {user_id: user.id}
+      delete api_v1_follow_path(other_user.id), params: {user_id: user.id}, as: :json
     end
     assert_response :success
   end
@@ -31,7 +31,7 @@ class Api::V1::FollowsControllerTest < ActionDispatch::IntegrationTest
   test "should not unfollow invalid follow" do
     user = User.create(name: "John")
     assert_no_difference('Follow.count') do
-      delete api_v1_follow_path(999), params: {user_id: user.id}
+      delete api_v1_follow_path(999), params: {user_id: user.id}, as: :json
     end
     assert_response :not_found
   end
